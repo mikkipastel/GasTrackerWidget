5df1668d7967f271335679615476ecfc0abbde47
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2024-11-09 15:46:54 +0700

    refactor

--- app/src/main/java/com/mikkipastel/gastracker/Utils.kt
+++ app/src/main/java/com/mikkipastel/gastracker/Utils.kt
@@ -1,11 +1,12 @@
 package com.mikkipastel.gastracker
 
-import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun String?.convertTo2Decimal(): String = String.format("%.2f", this?.toFloat())
+fun String?.convertTo2Decimal(): String = this?.toDouble().convertTo2Decimal()
+
+fun Double?.convertTo2Decimal(): String = "%.2f".format(this)
 
 fun getCurrentTimeStamp(): String {
     val simpleDate = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.forLanguageTag("TH"))
@@ -14,7 +15,7 @@ fun getCurrentTimeStamp(): String {
 
 fun calculateGasPriceUsd(ethPrice: String?, gasPrice: String?): String {
     val gasUnit = 21000
-    val gasEthPrice = BigDecimal(gasUnit * (gasPrice?.toInt() ?: 0)) * BigDecimal(0.000000001)
-    val result = (BigDecimal(ethPrice) * gasEthPrice)
-    return String.format("%.2f", result.toFloat())
+    val gasEthPrice = gasUnit.times(gasPrice?.toFloat() ?: 0f) * 0.000000001
+    val result = ethPrice?.toFloat()?.times(gasEthPrice)
+    return result.convertTo2Decimal()
 }
