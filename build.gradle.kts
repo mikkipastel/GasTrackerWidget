d6e63bdd1eb37b540b9e6fdbd4bac1c2cdc1cde0
Monthira Chayabanjonglerd <acaideddesign3@gmail.com>
2025-05-04 12:58:49 +0700

    update something

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     // Google services Gradle plugin 4.4.1+ dependency
     id("com.google.gms.google-services") version "4.4.2" apply false
     // Add the dependency for the Crashlytics Gradle plugin
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
     // Secret API Key
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
